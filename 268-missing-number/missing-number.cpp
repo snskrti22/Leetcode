2b16622cb5ada6bc8a6157dc05e48090d64511cc
snskrti22 <snskrti22@gmail.com>
2026-07-07 21:36:08 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (12.78%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -4,12 +4,10 @@ public:
         int n=0;
         int p=0;
        for(int i =0;i<=nums.size();i++) {
-       n=n+i;
-       
- }    for(int j=0;j<nums.size();j++){
-       p = p+nums[j];
- }
-      return n-p;
+       n=n+i;}
+       for(int j =0;j<nums.size();j++){
+        p=p+nums[j];
+       } return n-p;
 
 
     }
